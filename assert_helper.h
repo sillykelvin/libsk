ed84c4cd1b74419bdf9fed481379c880165dc845
Kelvin Hu <ini.kelvin@gmail.com>
2015-03-28 07:56:24 +0800

    common headers

---
+++ assert_helper.h
@@ -0,0 +1,25 @@
+#ifndef ASSERT_HELPER_H
+#define ASSERT_HELPER_H
+
+#include <assert.h>
+
+// TODO: enhance this macro
+#define ASSERT(exp) assert(exp)
+
+#define assert_retval(exp, val)                 \
+    do {                                        \
+        if (exp)                                \
+            break;                              \
+        ASSERT(exp);                            \
+        return val;                             \
+    } while (0)
+
+#define assert_retnone(exp)                     \
+    do {                                        \
+        if (exp)                                \
+            break;                              \
+        ASSERT(exp);                            \
+        return;                                 \
+    } while (0)
+
+#endif // ASSERT_HELPER_H
---
+++ types.h
@@ -0,0 +1,12 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <stdint.h>
+
+typedef uint64_t u64;
+
+typedef u64 shm_ptr;
+
+static const u64 SHM_NULL = (u64) -1;
+
+#endif // TYPES_H
