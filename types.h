c858d456f320b56a109c8f3b43e3f89f6b2123b5
Kelvin Hu <ini.kelvin@gmail.com>
2015-05-19 16:04:19 +0800

    change SHM_NULL from -1 to 0

--- types.h
+++ types.h
@@ -6,7 +6,7 @@ typedef uint32_t u32;
 
 typedef u64 shm_ptr;
 
-static const u64 SHM_NULL = (u64) -1;
+static const u64 SHM_NULL = (u64) 0;
 static const int IDX_NULL = (int) -1;
 
 #endif // TYPES_H
