25b45aa4c1176d651691a579e7dd3704e1d6e2a6
Kelvin Hu <ini.kelvin@gmail.com>
2015-05-19 11:15:51 +0800

    largely refine the performance testing program

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <vector>
+#include <unistd.h>
 #include "sk_inc.h"
 
-#define LOOP_COUNT 10000
+// NOTE: LOOP_COUNT must be 100 * N
+#define LOOP_COUNT 10000 // 1000000
 
 #define TINY_COUNT 1000
 #define HUGE_COUNT 100
 
-#define TINY_RANGE_MIN 32
-#define TINY_RANGE_MAX 1024
-#define HUGE_RANGE_MIN 1025
-#define HUGE_RANGE_MAX (1024 * 1024)
+#define SIZE_RANGE_MIN 32
+#define SIZE_RANGE_MAX (1024 * 1024)
+
+#define FIXED_SIZE_COUNT 3
 
 inline int rand_range(int min, int max) {
     if (min > max) {
@@ -21,155 +23,69 @@ inline int rand_range(int min, int max) {
     return  min == max ? min : (rand() % (max - min + 1) + min);
 }
 
-int test_new_delete(std::vector<char *>& tiny, std::vector<char *>& huge) {
-    for (int i = 0; i < LOOP_COUNT; ++i) {
-        for (int j = 0; j < TINY_COUNT; ++j) {
-            int size = rand_range(TINY_RANGE_MIN, TINY_RANGE_MAX);
-            char *addr = new char[size];
-            if (!addr) {
-                printf("new<size: %d> failed.", size);
-                return -1;
-            }
-
-            tiny.push_back(addr);
-        }
-
-        for (int j = 0; j < HUGE_COUNT; ++j) {
-            int size = rand_range(HUGE_RANGE_MIN, HUGE_RANGE_MAX);
-            char *addr = new char[size];
-            if (!addr) {
-                printf("new<size: %d> failed.", size);
-                return -1;
-            }
-
-            huge.push_back(addr);
-
-        }
-
-        for (int j = 0; j < (int) tiny.size(); ++j) {
-            delete [] tiny[j];
-        }
-
-        tiny.clear();
-
-        for (int j = 0; j < TINY_COUNT; ++j) {
-            int size = rand_range(TINY_RANGE_MIN, TINY_RANGE_MAX);
-            char *addr = new char[size];
-            if (!addr) {
-                printf("new<size: %d> failed.", size);
-                return -1;
-            }
-
-            tiny.push_back(addr);
-        }
-
-        for (int j = 0; j < (int) huge.size(); ++j) {
-            delete [] huge[j];
-        }
-
-        huge.clear();
-
-        for (int j = 0; j < HUGE_COUNT; ++j) {
-            int size = rand_range(HUGE_RANGE_MIN, HUGE_RANGE_MAX);
-            char *addr = new char[size];
-            if (!addr) {
-                printf("new<size: %d> failed.", size);
-                return -1;
-            }
-
-            huge.push_back(addr);
-
-        }
-
-        for (int j = 0; j < (int) tiny.size(); ++j) {
-            delete [] tiny[j];
-        }
-
-        for (int j = 0; j < (int) huge.size(); ++j) {
-            delete [] huge[j];
-        }
-
-        tiny.clear();
-        huge.clear();
-    }
-
-    return 0;
+inline bool rand_bool() {
+    return rand_range(0, 1) == 1;
 }
 
-int test_shm_mgr(std::vector<shm_ptr>& tiny, std::vector<shm_ptr>& huge) {
-    for (int i = 0; i < LOOP_COUNT; ++i) {
-        for (int j = 0; j < TINY_COUNT; ++j) {
-            int size = rand_range(TINY_RANGE_MIN, TINY_RANGE_MAX);
-            shm_ptr ptr;
-            void *addr = sk::shm_malloc(size, ptr);
-            if (!addr) {
-                printf("shm malloc<size: %d> failed.", size);
-                return -1;
-            }
+size_t random_size() {
+    return rand_range(SIZE_RANGE_MIN, SIZE_RANGE_MAX);
+}
 
-            tiny.push_back(ptr);
+size_t fixed_size() {
+    static bool first = true;
+    static size_t sizes[FIXED_SIZE_COUNT] = {0};
+
+    if (first) {
+        for (int i = 0; i < FIXED_SIZE_COUNT; ++i)
+            sizes[i] = rand_range(SIZE_RANGE_MIN, SIZE_RANGE_MAX);
+
+        first = false;
+    }
+
+    return sizes[rand_range(0, FIXED_SIZE_COUNT - 1)];
+}
+
+char *shm_new(size_t size, shm_ptr& ptr) {
+    void *addr = sk::shm_malloc(size, ptr);
+    return char_ptr(addr);
+}
+
+void shm_del(shm_ptr ptr) {
+    sk::shm_free(ptr);
+}
+
+char *heap_new(size_t size, char*& ptr) {
+    ptr = new char[size];
+    return ptr;
+}
+
+void heap_del(char *ptr) {
+    delete[] ptr;
+}
+
+template<typename P, size_t(*S)(), char*(*M)(size_t, P&), void(*F)(P)>
+int test_allocation_deallocation() {
+    std::vector<P> pointers;
+    pointers.reserve(LOOP_COUNT);
+
+    int c = 1;
+    while (c++ <= LOOP_COUNT) {
+        size_t size = S();
+        P p;
+        char *addr = M(size, p);
+        if (!addr) {
+            printf("allocation<size: %lu> failed.", size);
+            return -1;
         }
 
-        for (int j = 0; j < HUGE_COUNT; ++j) {
-            int size = rand_range(HUGE_RANGE_MIN, HUGE_RANGE_MAX);
-            shm_ptr ptr;
-            void *addr = sk::shm_malloc(size, ptr);
-            if (!addr) {
-                printf("shm malloc<size: %d> failed.", size);
-                return -1;
-            }
+        pointers.push_back(p);
 
-            huge.push_back(ptr);
+        if (c % 100 == 0) {
+            for (int i = 0; i < (int) pointers.size(); ++i)
+                F(pointers[i]);
 
+            pointers.clear();
         }
-
-        for (int j = 0; j < (int) tiny.size(); ++j) {
-            sk::shm_free(tiny[j]);
-        }
-
-        tiny.clear();
-
-        for (int j = 0; j < TINY_COUNT; ++j) {
-            int size = rand_range(TINY_RANGE_MIN, TINY_RANGE_MAX);
-            shm_ptr ptr;
-            void *addr = sk::shm_malloc(size, ptr);
-            if (!addr) {
-                printf("shm malloc<size: %d> failed.", size);
-                return -1;
-            }
-
-            tiny.push_back(ptr);
-        }
-
-        for (int j = 0; j < (int) huge.size(); ++j) {
-            sk::shm_free(huge[j]);
-        }
-
-        huge.clear();
-
-        for (int j = 0; j < HUGE_COUNT; ++j) {
-            int size = rand_range(HUGE_RANGE_MIN, HUGE_RANGE_MAX);
-            shm_ptr ptr;
-            void *addr = sk::shm_malloc(size, ptr);
-            if (!addr) {
-                printf("shm malloc<size: %d> failed.", size);
-                return -1;
-            }
-
-            huge.push_back(ptr);
-
-        }
-
-        for (int j = 0; j < (int) tiny.size(); ++j) {
-            sk::shm_free(tiny[j]);
-        }
-
-        for (int j = 0; j < (int) huge.size(); ++j) {
-            sk::shm_free(huge[j]);
-        }
-
-        tiny.clear();
-        huge.clear();
     }
 
     return 0;
@@ -188,37 +104,87 @@ void print_time_cost(const char *test_type, const timeval& begin, const timeval&
     printf("test type: %s;\t\t time cost: %f ms.\n", test_type, (float) (handle.tv_sec * 1000.0 + handle.tv_usec / 1000.0));
 }
 
-int main(int, char **) {
+void parse_parameters(int argc, char **argv,
+                      size_t& chunk_size, int& chunk_count, size_t& heap_size) {
+    chunk_size  = 0;
+    chunk_count = 0;
+    heap_size   = 0;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "s:c:h:")) != -1) {
+        switch (opt) {
+        case 's':
+            chunk_size = atol(optarg);
+            break;
+        case 'c':
+            chunk_count = atoi(optarg);
+            break;
+        case 'h':
+            heap_size = atol(optarg);
+            break;
+        default:
+            fprintf(stderr, "Usage: %s <-s chunk_size> <-c chunk_count> <-h heap_size>.\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (chunk_size == 0 || chunk_count == 0 || heap_size == 0) {
+        fprintf(stderr, "Usage: %s <-s chunk_size> <-c chunk_count> <-h heap_size>.\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char **argv) {
     srand(time(NULL));
 
-    sk::shm_mgr_init(0x7777, 0x777, 0x77, 0x7, false, TINY_RANGE_MAX + 128, TINY_COUNT + TINY_COUNT / 100, HUGE_COUNT * HUGE_RANGE_MAX);
+    size_t chunk_size = 0;
+    int chunk_count   = 0;
+    size_t heap_size  = 0;
+
+    parse_parameters(argc, argv, chunk_size, chunk_count, heap_size);
+
+    sk::shm_mgr_init(0x7777, 0x777, 0x77, 0x7, false, chunk_size, chunk_count, heap_size);
+
+    // sk::shm_mgr_init(0x7777, 0x777, 0x77, 0x7, false, 16, 1, SIZE_RANGE_MAX * 100);
+    // sk::shm_mgr_init(0x7777, 0x777, 0x77, 0x7, false, SIZE_RANGE_MAX, 100, SIZE_RANGE_MAX);
+    // sk::shm_mgr_init(0x7777, 0x777, 0x77, 0x7, false, 1024, 100, SIZE_RANGE_MAX * 100);
 
     timeval begin_time, end_time;
 
+    // 1. new/delete, random size
     {
-        std::vector<char *> tiny;
-        std::vector<char *> huge;
-        tiny.reserve(TINY_COUNT + TINY_COUNT / 100);
-        huge.reserve(HUGE_COUNT + HUGE_COUNT / 100);
-
         gettimeofday(&begin_time, NULL);
-        test_new_delete(tiny, huge);
+        test_allocation_deallocation<char *, random_size, heap_new, heap_del>();
         gettimeofday(&end_time, NULL);
 
-        print_time_cost("new/delete", begin_time, end_time);
+        print_time_cost("new/delete, random size", begin_time, end_time);
     }
 
+    // 2. new/delete, fixed size
     {
-        std::vector<shm_ptr> tiny;
-        std::vector<shm_ptr> huge;
-        tiny.reserve(TINY_COUNT + TINY_COUNT / 100);
-        huge.reserve(HUGE_COUNT + HUGE_COUNT / 100);
-
         gettimeofday(&begin_time, NULL);
-        test_shm_mgr(tiny, huge);
+        test_allocation_deallocation<char *, fixed_size, heap_new, heap_del>();
         gettimeofday(&end_time, NULL);
 
-        print_time_cost("shm_mgr", begin_time, end_time);
+        print_time_cost("new/delete, fixed size", begin_time, end_time);
+    }
+
+    // 3. shm mgr, random size
+    {
+        gettimeofday(&begin_time, NULL);
+        test_allocation_deallocation<shm_ptr, random_size, shm_new, shm_del>();
+        gettimeofday(&end_time, NULL);
+
+        print_time_cost("shm_mgr, random size", begin_time, end_time);
+    }
+
+    // 4. shm mgr, fixed size
+    {
+        gettimeofday(&begin_time, NULL);
+        test_allocation_deallocation<shm_ptr, fixed_size, shm_new, shm_del>();
+        gettimeofday(&end_time, NULL);
+
+        print_time_cost("shm_mgr, fixed size", begin_time, end_time);
     }
 
     sk::shm_mgr_fini();
