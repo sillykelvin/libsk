2c2edc9bc8981245b53e0fbbe1961233705d2a88
Kelvin Hu <ini.kelvin@gmail.com>
2015-07-03 16:08:40 +0800

    add unit test

---
+++ test/test_referable_array.cpp
@@ -0,0 +1,68 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include "sk_inc.h"
+
+#define MAX_SIZE 5
+
+using namespace std;
+using namespace sk;
+
+struct rarray_test {
+    int i;
+    size_t waste;
+
+    rarray_test() : i(0) { cout << "ctor called" << endl; }
+    ~rarray_test() { cout << "dtor called, i: " << i << endl; }
+};
+
+typedef referable_array<rarray_test, MAX_SIZE> array;
+
+TEST(referable_array, normal) {
+    array ta;
+
+    ASSERT_TRUE(ta.empty());
+    ASSERT_TRUE(ta.size() == 0);
+    ASSERT_TRUE(ta.capacity() == MAX_SIZE);
+
+    for (size_t i = 0; i < ta.capacity(); ++i) {
+        rarray_test *t = ta.emplace();
+        ASSERT_TRUE(t);
+
+        t->i = i;
+    }
+
+    ASSERT_TRUE(ta.full());
+    ASSERT_TRUE(ta.emplace() == NULL);
+
+    rarray_test *t = ta.at(3);
+    ASSERT_TRUE(t);
+    ASSERT_TRUE(t->i == 3);
+    t = ta.at(2);
+    ASSERT_TRUE(t);
+    ASSERT_TRUE(t->i == 2);
+
+    ta.erase(3);
+    ta.erase(2);
+    ASSERT_TRUE(ta.size() == MAX_SIZE - 2);
+    ASSERT_TRUE(!ta.full());
+    ASSERT_TRUE(!ta.empty());
+    ASSERT_TRUE(ta.at(3) == NULL);
+    ASSERT_TRUE(ta.at(2) == NULL);
+
+    t = ta.emplace();
+    t->i = 6;
+    ASSERT_TRUE(ta.at(2)->i == 6);
+    t->i = 7;
+    ASSERT_TRUE(ta.at(2)->i == 7);
+    t = ta.emplace();
+    t->i = 8;
+    ASSERT_TRUE(ta.at(3)->i == 8);
+    ASSERT_TRUE(ta.full());
+
+    ta.erase(1);
+    ta.clear();
+    ASSERT_TRUE(ta.empty());
+    ta.emplace()->i = 9;
+    ASSERT_TRUE(ta.at(4));
+    ASSERT_TRUE(ta.at(4)->i == 9);
+}
