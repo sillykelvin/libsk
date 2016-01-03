ca4613c5a9e33f1c6fd6540f5495e4fbad888123
Kelvin Hu <ini.kelvin@gmail.com>
2016-01-03 13:29:43 +0800

    re-implement metadata_allocator

--- src/shm/detail/metadata_allocator.h
+++ src/shm/detail/metadata_allocator.h
@@ -9,9 +9,9 @@ namespace detail {
  */
 template<typename T>
 struct metadata_allocator {
-    offset_t free_list;
-    offset_t free_space;
-    size_t   space_left;
+    shm_ptr<T> free_list;
+    offset_t   free_space;
+    size_t     space_left;
 
     struct {
         size_t total_size;  // total size allocated from shared memory
@@ -22,54 +22,51 @@ struct metadata_allocator {
 
     void init() {
         assert_noeffect(sizeof(T) <= META_ALLOC_INCREMENT);
-        free_list = OFFSET_NULL;
+        free_list = SHM_NULL;
         free_space = OFFSET_NULL;
         space_left = 0;
 
         memset(&stat, 0x00, sizeof(stat));
     }
 
-    T *allocate(offset_t *offset) {
+    shm_ptr<T> allocate() {
         // 1. if there are objects in free list
-        if (free_list != OFFSET_NULL) {
-            // TODO: may replace with shm_ptr here, same as below
-            void *addr = shm_mgr::get()->offset2ptr(free_list);
-            if (offset)
-                *offset = free_list;
-
-            free_list = *cast_ptr(offset_t, addr);
+        if (free_list) {
+            shm_ptr<T> ret = free_list;
+            free_list = *cast_ptr(shm_ptr<T>, free_list.get());
             stat.alloc_count += 1;
-
-            return cast_ptr(T, addr);
+            return ret;
         }
 
         // 2. if no enough space in free space
         if (space_left < sizeof(T)) {
+            shm_ptr<void> ptr = shm_mgr::get()->allocate_metadata(META_ALLOC_INCREMENT);
+            if (!ptr)
+                return SHM_NULL;
+
             stat.waste_size += space_left;
-
             space_left = META_ALLOC_INCREMENT;
-            void *addr = shm_mgr::get()->allocate_metadata(space_left, &free_space);
-            if (!addr)
-                return NULL;
-
+            free_space = ptr.offset;
             stat.total_size += space_left;
         }
 
-        void *addr = shm_mgr::get()->offset2ptr(free_space);
-        if (offset)
-            *offset = free_space;
+        shm_ptr<T> ptr(free_space);
 
         space_left -= sizeof(T);
         free_space += sizeof(T);
         stat.alloc_count += 1;
 
-        return cast_ptr(T, addr);
+        return ptr;
     }
 
-    void deallocate(offset_t offset) {
-        void *addr = shm_mgr::get()->offset2ptr(offset);
-        *cast_ptr(offset_t, addr) = free_list;
-        free_list = offset;
+    void deallocate(shm_ptr<T> ptr) {
+        if (!ptr) {
+            assert_noeffect(0);
+            return;
+        }
+
+        *cast_ptr(shm_ptr<T>, ptr.get()) = free_list;
+        free_list = ptr;
         stat.free_count += 1;
     }
 };
