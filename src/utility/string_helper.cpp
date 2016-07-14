be74224568985cf53bc3d80075adf12b96d2f407
Kelvin Hu <-l>
2016-07-14 21:30:00 +0800

    fix compilation error

--- src/utility/string_helper.cpp
+++ src/utility/string_helper.cpp
@@ -3,13 +3,12 @@
 
 namespace sk {
 
-template<typename T, T(*func)(const char *, char **, int)>
-int string2type(const char *str, T& val) {
+int string2s32(const char *str, s32& val) {
     if (!str) return -EINVAL;
 
     errno = 0;
     char *end = NULL;
-    val = func(str, &end, 10);
+    val = strtol(str, &end, 10);
 
     if (errno || *end != '\0' || end == str)
         return -EINVAL;
@@ -17,20 +16,43 @@ int string2type(const char *str, T& val) {
     return 0;
 }
 
-int string2s32(const char *str, s32& val) {
-    return string2type<s32, strtol>(str, val);
-}
-
 int string2u32(const char *str, u32& val) {
-    return string2type<s32, strtoul>(str, val);
+    if (!str) return -EINVAL;
+
+    errno = 0;
+    char *end = NULL;
+    val = strtoul(str, &end, 10);
+
+    if (errno || *end != '\0' || end == str)
+        return -EINVAL;
+
+    return 0;
 }
 
 int string2s64(const char *str, s64& val) {
-    return string2type<s32, strtoll>(str, val);
+    if (!str) return -EINVAL;
+
+    errno = 0;
+    char *end = NULL;
+    val = strtoll(str, &end, 10);
+
+    if (errno || *end != '\0' || end == str)
+        return -EINVAL;
+
+    return 0;
 }
 
 int string2u64(const char *str, u64& val) {
-    return string2type<s32, strtoull>(str, val);
+    if (!str) return -EINVAL;
+
+    errno = 0;
+    char *end = NULL;
+    val = strtoull(str, &end, 10);
+
+    if (errno || *end != '\0' || end == str)
+        return -EINVAL;
+
+    return 0;
 }
 
 } // namespace sk
