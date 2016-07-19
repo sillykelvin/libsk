36d006b1586b055d4f09465966b3f19a6a869da6
Kelvin Hu <-l>
2016-07-19 21:36:09 +0800

    refine string helper functions

--- src/utility/string_helper.cpp
+++ src/utility/string_helper.cpp
@@ -3,82 +3,56 @@
 
 namespace sk {
 
-int string2s32(const char *str, s32& val) {
+template<typename T, typename R, R(*func)(const char *, char **, int)>
+static int convert(const char *str, T& val) {
     if (!str) return -EINVAL;
 
     errno = 0;
     char *end = NULL;
-    val = strtol(str, &end, 10);
+    val = func(str, &end, 10);
 
     if (errno || *end != '\0' || end == str)
         return -EINVAL;
 
     return 0;
+}
+
+template<typename T, typename R, R(*func)(const char *, char **)>
+static int convert(const char *str, T& val) {
+    if (!str) return -EINVAL;
+
+    errno = 0;
+    char *end = NULL;
+    val = func(str, &end);
+
+    if (errno || *end != '\0' || end == str)
+        return -EINVAL;
+
+    return 0;
+}
+
+int string2s32(const char *str, s32& val) {
+    return convert<s32, long, strtol>(str, val);
 }
 
 int string2u32(const char *str, u32& val) {
-    if (!str) return -EINVAL;
-
-    errno = 0;
-    char *end = NULL;
-    val = strtoul(str, &end, 10);
-
-    if (errno || *end != '\0' || end == str)
-        return -EINVAL;
-
-    return 0;
+    return convert<u32, unsigned long, strtoul>(str, val);
 }
 
 int string2s64(const char *str, s64& val) {
-    if (!str) return -EINVAL;
-
-    errno = 0;
-    char *end = NULL;
-    val = strtoll(str, &end, 10);
-
-    if (errno || *end != '\0' || end == str)
-        return -EINVAL;
-
-    return 0;
+    return convert<s64, long long, strtoll>(str, val);
 }
 
 int string2u64(const char *str, u64& val) {
-    if (!str) return -EINVAL;
-
-    errno = 0;
-    char *end = NULL;
-    val = strtoull(str, &end, 10);
-
-    if (errno || *end != '\0' || end == str)
-        return -EINVAL;
-
-    return 0;
+    return convert<u64, unsigned long long, strtoull>(str, val);
 }
 
 int string2float(const char *str, float &val) {
-    if (!str) return -EINVAL;
-
-    errno = 0;
-    char *end = NULL;
-    val = strtof(str, &end);
-
-    if (errno || *end != '\0' || end == str)
-        return -EINVAL;
-
-    return 0;
+    return convert<float, float, strtof>(str, val);
 }
 
 int string2double(const char *str, double &val) {
-    if (!str) return -EINVAL;
-
-    errno = 0;
-    char *end = NULL;
-    val = strtod(str, &end);
-
-    if (errno || *end != '\0' || end == str)
-        return -EINVAL;
-
-    return 0;
+    return convert<double, double, strtod>(str, val);
 }
 
 } // namespace sk
