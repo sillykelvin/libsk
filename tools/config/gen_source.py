abbcfb7d5175cebbbde54ae20c7331202068cc59
Kelvin Hu <-l>
2016-07-19 17:46:25 +0800

    finally the config loader generator works

--- tools/config/gen_source.py
+++ tools/config/gen_source.py
@@ -5,31 +5,32 @@ import sys
 import string
 
 
-def enum(**enums):
-    return type('Enum', (), enums)
-
-field_type = enum(
-    s32    = 's32',
-    u32    = 'u32',
-    s64    = 's64',
-    u64    = 'u64',
-    string = 'string',
-    vector = 'vector',
-    custom = 'custom'
-)
+FIELD_DEFS = {
+    's32'    : {'type': 's32', 'conv_func': 'sk::string2s32'},
+    'u32'    : {'type': 'u32', 'conv_func': 'sk::string2u32'},
+    's64'    : {'type': 's64', 'conv_func': 'sk::string2s64'},
+    'u64'    : {'type': 'u64', 'conv_func': 'sk::string2u64'},
+    'string' : {'type': 'string', 'conv_func': None},
+    'vector' : {'type': 'vector', 'conv_func': None},
+    'custom' : {'type': 'custom', 'conv_func': None},
+}
 
 class ConfigDef:
     def __init__(self):
-        self.name = None
-        self.fields = []
+        self.name    = None
+        self.prefix  = None  # prefix namespace
+        self.fields  = []
         self.sub_def = []    # children definitions
         self.sup_def = None  # parent definition
 
     def __str__(self):
         return self.info(0)
 
+    def full_name(self):
+        return self.prefix + self.name
+
     def info(self, indent):
-        ret = '%sDEFINITION: %s\n%sfields:\n' % (' ' * indent, self.name, ' ' * indent)
+        ret = '%sDEFINITION: %s\n%sfields:\n' % (' ' * indent, self.full_name(), ' ' * indent)
         for f in self.fields:
             ret += f.info(indent + 4)
 
@@ -40,58 +41,92 @@ class ConfigDef:
 
 class FieldInfo:
     def __init__(self):
-        self.field_type = None
-        self.field_name = None
-        # if the field is std::vector, sub_type is the type in vector
-        # if the field is customized type, sub_type is that type
-        self.sub_type   = None
+        self._def  = None        # which def this field belongs
+        self._type = None
+        self.name  = None
+        # will be set only if _type is vector of customized type
+        # if the field is std::vector, real_type is the type in vector, if it is scalar
+        # type, real_type is a string, otherwise, real_type referenced to ConfigDef instance
+        # if the field is customized type, real_type is that type, referenced to ConfigDef instance
+        self.real_type = None
 
     def __str__(self):
         return info(0)
 
     def info(self, indent):
-        ret = '%stype: %s, name: %s' % (' ' * indent, self.field_type, self.field_name)
-        if self.field_type in (field_type.vector, field_type.custom):
-            assert self.sub_type
-            ret += ', sub_type: %s' % (self.sub_type)
+        ret = '%stype: %s, name: %s' % (' ' * indent, self._type, self.name)
+        if self._type['type'] in ('vector', 'custom'):
+            assert self.real_type
+            if isinstance(self.real_type, str):
+                ret += ', real_type: %s' % (self.real_type)
+            else:
+                assert isinstance(self.real_type, ConfigDef)
+                ret += ', real_type: %s' % (self.real_type.full_name())
         ret += '\n'
 
         return ret
 
-def build_config_def(root_lists, parent, name):
+def find_config_def(root_defs, sup_def, type_str):
+    for d in sup_def.sub_def:
+        if d.name == type_str:
+            return d
+
+    if sup_def.sup_def:
+        return find_config_def(root_defs, sup_def.sup_def, type_str)
+
+    for d in root_defs:
+        if d.name == type_str:
+            return d
+
+    return None
+
+def build_config_def(root_defs, parent, name):
     _def = ConfigDef()
     _def.name = name
     if parent:
         _def.sup_def = parent
+        _def.prefix = parent.full_name() + '::'
         parent.sub_def.append(_def)
     else:
-        root_lists.append(_def)
+        _def.prefix = ''
+        root_defs.append(_def)
 
     return _def
 
-def build_field_info(line, type_str, name):
+def build_field_info(root_defs, sup_def, type_str, name):
+    assert sup_def
     field = FieldInfo()
-    field.field_name = name
+    field._def = sup_def
+    field.name = name
 
-    if type_str == 'int' or type_str == 's32':
-        field.field_type = field_type.s32
-    elif type_str == 'u32':
-        field.field_type = field_type.u32
-    elif type_str == 's64':
-        field.field_type = field_type.s64
-    elif type_str == 'u64':
-        field.field_type = field_type.u64
+    if type_str == 'int':
+        type_str = 's32'
+
+    if len(type_str) == 3 and type_str in FIELD_DEFS.keys():
+        field._type = FIELD_DEFS[type_str]
     elif type_str == 'std::string':
-        field.field_type = field_type.string
+        field._type = FIELD_DEFS['string']
     elif type_str.startswith('std::vector<'):
-        field.field_type = field_type.vector
+        field._type = FIELD_DEFS['vector']
         ret = re.search('^std::vector< *([_a-zA-Z0-9]+) *>$', type_str)
         assert ret
-        field.sub_type = ret.group(1)
+        real_type = ret.group(1)
+        if real_type in ('int', 's32', 'u32', 's64', 'u64', 'std::string'):
+            if real_type == 'int':
+                real_type = 's32'
+            field.real_type = real_type
+        else:
+            field.real_type = find_config_def(root_defs, sup_def, ret.group(1))
+            if not field.real_type:
+                print 'cannot find type: %s' % (ret.group(1))
+                sys.exit(-1)
     else:
         # customize types
-        field.field_type = field_type.custom
-        field.sub_type = type_str
+        field._type = FIELD_DEFS['custom']
+        field.real_type = find_config_def(root_defs, sup_def, type_str)
+        if not field.real_type:
+            print 'cannot find type: %s' % (type_str)
+            sys.exit(-1)
 
     return field
 
@@ -154,7 +189,7 @@ def parse_content(content):
         if ret:
             type_str = ret.group(1)
             name = ret.group(2)
-            field = build_field_info(line, type_str, name)
+            field = build_field_info(def_list, curr_def, type_str, name)
             curr_def.fields.append(field)
             continue
 
@@ -163,127 +198,198 @@ def parse_content(content):
 
     return def_list
 
-def write_string_field(field, conf_name):
-    template = '''
-    pugi::xml_node $field_name = $conf_name.child("$field_name");
-    if (!$field_name) {
-        fprintf(stderr, "node $field_name not found.", $field_name);
+def write_string_load_func():
+    return '''
+static int load_from_xml_node(std::string& value, const pugi::xml_node& node, const char *node_name) {
+    value.clear();
+    if (!node) {
+        fprintf(stderr, "node %s not found.", node_name);
         return -EINVAL;
     }
-    conf.$field_name = $field_name.text().get();
-'''
-
-    t = string.Template(template)
-    return t.substitute(conf_name = conf_name, field_name = field.field_name)
-
-def write_integral_field(field, conf_name):
-    template = '''
-    pugi::xml_node $field_name = $conf_name.child("$field_name");
-    if (!$field_name) {
-        fprintf(stderr, "node $field_name not found.", $field_name);
-        return -EINVAL;
-    }
-    ret = sk::$func_name($field_name.text().get(), conf.$field_name);
-    if (ret != 0) {
-        fprintf(stderr, "cannot convert node $field_name.", $field_name);
-        return ret;
-    }
-'''
-    func = 'string2%s' % (field.field_type)
-    t = string.Template(template)
-    return t.substitute(conf_name = conf_name,
-                        field_name = field.field_name,
-                        func_name = func)
-
-def write_vector_field(field, conf_name):
-    template = '''
-    auto ${field_name}_set = $conf_name.children("$field_name");
-    for (auto it = ${field_name}_set.begin(), end = ${field_name}_set.end(); it != end; ++it) {
-        const xml_node n = *it;
-        $field_type obj;
-        ret = load_node(obj, n);
-        if (ret != 0)
-            return ret;
-
-        conf.$field_name.push_back(obj);
-    }
-    if (conf.$field_name.size() <= 0) {
-        fprintf(stderr, "node $field_name not found.", $field_name);
-        return -EINVAL;
-    }
-'''
-
-    t = string.Template(template)
-    return t.substitute(conf_name = conf_name,
-                        field_name = field.field_name,
-                        field_type = field.sub_type)
-
-def write_custom_field(field, conf_name):
-    template = '''
-    auto ${field_name} = $conf_name.child("$field_name");
-    if (!$field_name) {
-        fprintf(stderr, "node $field_name not found.", $field_name);
-        return -EINVAL;
-    }
-    ret = load_node(conf.$field_name, $field_name);
-    if (ret != 0)
-        return ret;
-'''
-
-    t = string.Template(template)
-    return t.substitute(conf_name = conf_name,
-                        field_name = field.field_name)
-
-def write_source(def_list, filename):
-    template = '''
-int load_file($conf_name& conf, const char *file) {
-    assert_retval(file, -1);
-
-    int ret = 0;
-    pugi::xml_document doc;
-    auto ok = doc.load_file(file);
-    if (!ok) {
-        fprintf(stderr, "load file<%s> error: %s.", file, ok.description());
-        return -EINVAL;
-    }
-
-    pugi::xml_node $conf_name = doc.child("$conf_name");
-    if (!$conf_name) {
-        fprintf(stderr, "node $conf_name not found.", $conf_name, file);
-        return -EINVAL;
-    }
-
-    // read fields from xml
-$fields_src
+    value = node.text().get();
 
     return 0;
 }
 '''
-    t = string.Template(template)
+
+def write_integral_load_func(type_str):
+    assert len(type_str) == 3
+    assert type_str in FIELD_DEFS.keys()
+    template = '''
+static int load_from_xml_node(${type_name}& value, const pugi::xml_node& node, const char *node_name) {
+    value = 0;
+    if (!node) {
+        fprintf(stderr, "node %s not found.", node_name);
+        return -EINVAL;
+    }
+
+    int ret = $func_name(node.text().get(), value);
+    if (ret != 0) {
+        fprintf(stderr, "cannot convert node %s to integer.", node_name);
+        return ret;
+    }
+
+    return 0;
+}
+'''
+    return string.Template(template).substitute(type_name = FIELD_DEFS[type_str]['type'],
+                                                func_name = FIELD_DEFS[type_str]['conv_func'])
+
+def write_vector_load_func(real_type):
+    template = '''
+static int load_from_xml_node(std::vector<$type_name>& value, const pugi::xml_object_range<pugi::xml_named_node_iterator>& nodes, const char *node_name) {
+    value.clear();
+    for (auto it = nodes.begin(), end = nodes.end(); it != end; ++it) {
+        const pugi::xml_node n = *it;
+        $type_name obj;
+        int ret = load_from_xml_node(obj, n, node_name);
+        if (ret != 0)
+            return ret;
+
+        value.push_back(obj);
+    }
+    if (value.size() <= 0) {
+        fprintf(stderr, "node %s not found.", node_name);
+        return -EINVAL;
+    }
+
+    return 0;
+}
+'''
+
+    type_name = None
+    if isinstance(real_type, str):
+        type_name = real_type
+    else:
+        assert isinstance(real_type, ConfigDef)
+        type_name = real_type.full_name()
+
+    return string.Template(template).substitute(type_name = type_name);
+
+def write_custom_load_func(_def):
+    func_template = '''
+static int load_from_xml_node($type_name& value, const pugi::xml_node& node, const char *node_name) {
+    int ret = 0;
+    if (!node) {
+        fprintf(stderr, "node %s not found.", node_name);
+        return -EINVAL;
+    }
+
+    // read fields
+$fields_src
+    return 0;
+}
+'''
+
+    field_template = '''
+    ret = load_from_xml_node(value.$field_name, $node_arg, "$field_name");
+    if (ret != 0)
+        return ret;
+'''
+
+    fields_src = ''
+    for field in _def.fields:
+        node_arg = 'node.child("%s")' % (field.name)
+        if field._type['type'] == 'vector':
+            node_arg = 'node.children("%s")' % (field.name)
+
+        fields_src += string.Template(field_template).substitute(field_name = field.name, node_arg = node_arg)
+
+    return string.Template(func_template).substitute(type_name = _def.full_name(), fields_src = fields_src)
+
+def extract_all_types(def_list):
+    def recursive(_def, type_dict):
+        type_dict[_def.full_name()] = {'type': 'custom', 'real_type': _def}
+
+        for f in _def.fields:
+            if f._type['type'] == 'string':
+                type_dict['std::string'] = {'type': 'string', 'real_type': None}
+            elif f._type['type'] == 'vector':
+                assert f.real_type
+                if isinstance(f.real_type, str):
+                    assert f.real_type in ('s32', 'u32', 's64', 'u64', 'std::string')
+                    inner_type = f.real_type
+                    if f.real_type == 'std::string':
+                        inner_type = 'string'
+                    type_dict[f.real_type] = {'type': inner_type, 'real_type': None}
+
+                    type_dict['std::vector<%s>' % (f.real_type)] = {'type': 'vector', 'real_type': f.real_type}
+                else:
+                    assert isinstance(f.real_type, ConfigDef)
+                    type_dict[f.real_type.full_name()] = {'type': 'custom', 'real_type': f.real_type}
+                    type_dict['std::vector<%s>' % (f.real_type.full_name())] = {'type': 'vector', 'real_type': f.real_type}
+            elif f._type['type'] == 'custom':
+                assert f.real_type
+                assert isinstance(f.real_type, ConfigDef)
+                type_dict[f.real_type.full_name()] = {'type': 'custom', 'real_type': f.real_type}
+            else: # integral types
+                type_dict[f._type['type']] = {'type': f._type['type'], 'real_type': None}
+
+        for d in _def.sub_def:
+            recursive(d, type_dict)
+
+    type_dict = {}
+    for d in def_list:
+        recursive(d, type_dict)
+
+    return type_dict
+
+def write_declarations(type_dict):
+    ret = ''
+    for t in type_dict.keys():
+        if type_dict[t]['type'] == 'vector':
+            ret += 'static int load_from_xml_node(%s& value, const pugi::xml_object_range<pugi::xml_named_node_iterator>& nodes, const char *node_name);\n' % (t)
+        else:
+            ret += 'static int load_from_xml_node(%s& value, const pugi::xml_node& node, const char *node_name);\n' % (t)
+
+    return ret
+
+def write_source(def_list, filename):
+    template = '''
+int load_from_xml_file($conf_name& conf, const char *file) {
+    assert_retval(file, -1);
+
+    pugi::xml_document doc;
+    auto ok = doc.load_file(file);
+    if (!ok) {
+        fprintf(stderr, "load file %s error: %s.", file, ok.description());
+        return -EINVAL;
+    }
+
+    pugi::xml_node root = doc.child("$conf_name");
+    if (!root) {
+        fprintf(stderr, "root node <$conf_name> not found.");
+        return -EINVAL;
+    }
+
+    return load_from_xml_node(conf, root, "$conf_name");
+}
+'''
 
     with open(filename, 'w') as f:
         f.write('// Generated by tool, DO NOT EDIT!!!\n\n')
 
+        type_dict = extract_all_types(def_list)
+
+        # forward declarations
+        f.write(write_declarations(type_dict))
+        f.write('\n')
+
+        # implementations
+        for t in type_dict:
+            if type_dict[t]['type'] == 'string':
+                f.write(write_string_load_func())
+            elif type_dict[t]['type'] == 'vector':
+                f.write(write_vector_load_func(type_dict[t]['real_type']))
+            elif type_dict[t]['type'] == 'custom':
+                assert isinstance(type_dict[t]['real_type'], ConfigDef)
+                f.write(write_custom_load_func(type_dict[t]['real_type']))
+            else: # integral types
+                f.write(write_integral_load_func(t))
+
+        # only first level definitions can be loaded from file
         for _def in def_list:
-            fields_src = ''
-            for field in _def.fields:
-                if field.field_type == field_type.s32:
-                    fields_src += write_integral_field(field, _def.name)
-                elif field.field_type == field_type.u32:
-                    fields_src += write_integral_field(field, _def.name)
-                elif field.field_type == field_type.s64:
-                    fields_src += write_integral_field(field, _def.name)
-                elif field.field_type == field_type.u64:
-                    fields_src += write_integral_field(field, _def.name)
-                elif field.field_type == field_type.string:
-                    fields_src += write_string_field(field, _def.name)
-                elif field.field_type == field_type.vector:
-                    fields_src += write_vector_field(field, _def.name)
-                elif field.field_type == field_type.custom:
-                    fields_src += write_custom_field(field, _def.name)
-
-            f.write(t.substitute(conf_name = _def.name, fields_src = fields_src))
-
+            f.write(string.Template(template).substitute(conf_name = _def.name))
 
 if __name__ == '__main__':
     argc = len(sys.argv)
