afd06d18111bc796ad7c32fde5b8a66442c6a231
Kelvin Hu <-l>
2016-07-19 21:44:12 +0800

    totally refine string helper functions

--- src/utility/string_helper.cpp
+++ src/utility/string_helper.cpp
@@ -3,27 +3,39 @@
 
 namespace sk {
 
-template<typename T, typename R, R(*func)(const char *, char **, int)>
-static int convert(const char *str, T& val) {
+template<typename R, bool integral>
+struct functor {
+    R(*f_)(const char *, char **, int);
+
+    functor(R(*func)(const char *, char **, int)) {
+        f_ = func;
+    }
+
+    R operator()(const char *str, char **end) const {
+        return f_(str, end, 10);
+    }
+};
+
+template<typename R>
+struct functor<R, false> {
+    R(*f_)(const char *, char **);
+
+    functor(R(*func)(const char *, char **)) {
+        f_ = func;
+    }
+
+    R operator()(const char *str, char **end) const {
+        return f_(str, end);
+    }
+};
+
+template<typename T, typename R, bool integral>
+static int convert(const char *str, T& val, const functor<R, integral>& f) {
     if (!str) return -EINVAL;
 
     errno = 0;
     char *end = NULL;
-    val = func(str, &end, 10);
-
-    if (errno || *end != '\0' || end == str)
-        return -EINVAL;
-
-    return 0;
-}
-
-template<typename T, typename R, R(*func)(const char *, char **)>
-static int convert(const char *str, T& val) {
-    if (!str) return -EINVAL;
-
-    errno = 0;
-    char *end = NULL;
-    val = func(str, &end);
+    val = f(str, &end);
 
     if (errno || *end != '\0' || end == str)
         return -EINVAL;
@@ -32,27 +44,33 @@ static int convert(const char *str, T& val) {
 }
 
 int string2s32(const char *str, s32& val) {
-    return convert<s32, long, strtol>(str, val);
+    functor<long, true> f(strtol);
+    return convert(str, val, f);
 }
 
 int string2u32(const char *str, u32& val) {
-    return convert<u32, unsigned long, strtoul>(str, val);
+    functor<unsigned long, true> f(strtoul);
+    return convert(str, val, f);
 }
 
 int string2s64(const char *str, s64& val) {
-    return convert<s64, long long, strtoll>(str, val);
+    functor<long long, true> f(strtoll);
+    return convert(str, val, f);
 }
 
 int string2u64(const char *str, u64& val) {
-    return convert<u64, unsigned long long, strtoull>(str, val);
+    functor<unsigned long long, true> f(strtoull);
+    return convert(str, val, f);
 }
 
 int string2float(const char *str, float &val) {
-    return convert<float, float, strtof>(str, val);
+    functor<float, false> f(strtof);
+    return convert(str, val, f);
 }
 
 int string2double(const char *str, double &val) {
-    return convert<double, double, strtod>(str, val);
+    functor<double, false> f(strtod);
+    return convert(str, val, f);
 }
 
 } // namespace sk
