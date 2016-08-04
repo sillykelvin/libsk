98f50066ea6a9b127496bf43ce7573a67075935c
Kelvin Hu <-l>
2016-08-04 15:41:26 +0800

    add murmurhash3 implementation

---
+++ src/common/murmurhash3.h
@@ -0,0 +1,20 @@
+#ifndef MURMURHASH3_H
+#define MURMURHASH3_H
+
+/*
+ * These hash algorithms are copied from the following:
+ *     https://github.com/aappleby/smhasher/wiki/MurmurHash3
+ * Thanks to the author for the great job.
+ */
+
+#include "utility/types.h"
+
+namespace sk {
+
+void murmurhash3_x86_32 (const void *key, size_t len, u32 seed, u32 out[1]);
+void murmurhash3_x86_128(const void *key, size_t len, u32 seed, u32 out[4]);
+void murmurhash3_x64_128(const void *key, size_t len, u32 seed, u64 out[2]);
+
+} // namespace sk
+
+#endif // MURMURHASH3_H
