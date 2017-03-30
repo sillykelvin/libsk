5722c03ba87ec074f4829ec300034ba83425f80c
Kelvin Hu <ini.kelvin@gmail.com>
2017-03-30 18:27:56 +0800

    add class inet_address

---
+++ src/net/inet_address.cpp
@@ -0,0 +1,40 @@
+#include <string.h>
+#include <arpa/inet.h>
+#include "inet_address.h"
+#include "utility/assert_helper.h"
+
+NS_BEGIN(sk)
+
+inet_address::inet_address(u16 port) {
+    memset(&addr_, 0x00, sizeof(addr_));
+
+    addr_.sin_family = AF_INET;
+    addr_.sin_port = htons(port);
+    addr_.sin_addr.s_addr = INADDR_ANY;
+}
+
+inet_address::inet_address(const std::string& ip, u16 port) {
+    memset(&addr_, 0x00, sizeof(addr_));
+
+    addr_.sin_family = AF_INET;
+    addr_.sin_port = htons(port);
+    int ret = inet_pton(AF_INET, ip.c_str(), &addr_.sin_addr);
+    sk_assert(ret == 0);
+}
+
+inet_address::inet_address(const sockaddr_in& addr) {
+    memcpy(&addr_, &addr, sizeof(addr_));
+}
+
+std::string inet_address::to_string() const {
+    char addr[64] = {0};
+    char host[32] = "INVALID";
+
+    u16 port = ntohs(addr_.sin_port);
+    inet_ntop(AF_INET, &addr_.sin_addr, host, sizeof(host));
+    snprintf(addr, sizeof(addr), "%s:%u", host, port);
+
+    return addr;
+}
+
+NS_END(sk)
---
+++ src/net/inet_address.h
@@ -0,0 +1,28 @@
+#ifndef INET_ADDRESS_H
+#define INET_ADDRESS_H
+
+#include <string>
+#include <netdb.h>
+#include "utility/types.h"
+
+NS_BEGIN(sk)
+
+class inet_address {
+public:
+    explicit inet_address(u16 port);
+    inet_address(const std::string& ip, u16 port);
+    inet_address(const struct sockaddr_in& addr);
+
+    struct sockaddr *address() {
+        return cast_ptr(sockaddr, &addr_);
+    }
+
+    std::string to_string() const;
+
+private:
+    struct sockaddr_in addr_;
+};
+
+NS_END(sk)
+
+#endif // INET_ADDRESS_H
