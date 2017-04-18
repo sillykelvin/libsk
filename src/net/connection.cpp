895bb8ac8d7ad442e960b966fdc6532b3983b11b
Kelvin Hu <ini.kelvin@gmail.com>
2017-04-18 18:13:32 +0800

    implement connection class

--- src/net/connection.cpp
+++ src/net/connection.cpp
@@ -1,29 +1,129 @@
 #include "connection.h"
+#include "net/reactor.h"
 #include "utility/assert_helper.h"
 
 NS_BEGIN(sk)
 
-handler_ptr connection::create(reactor *r) {
-    return std::shared_ptr<connection>(new connection(r));
+connection_ptr connection::create(reactor *r,
+                                  const socket_ptr& socket,
+                                  const inet_address& local_addr,
+                                  const inet_address& remote_addr) {
+    return std::make_shared<connection>(r, socket, local_addr, remote_addr);
 }
 
-void connection::set_socket(const socket_ptr& socket) {
-    sk_assert(!socket_);
-    socket_ = socket;
-}
+connection::connection(reactor *r,
+                       const socket_ptr& socket,
+                       const inet_address& local_addr,
+                       const inet_address& remote_addr)
+    : state_(state_disconnected),
+      handler(r),
+      socket_(socket),
+      local_addr_(local_addr),
+      remote_addr_(remote_addr)
+{}
 
-int connection::send(const void *data, size_t len) {
-    // TODO
-    return 0;
+ssize_t connection::send(const void *data, size_t len) {
+    if (state_ != state_connected) {
+        sk_error("wrong state<%d>.", state_);
+        return -1;
+    }
+
+    // empty output queue, just write directly
+    ssize_t nbytes = 0;
+    size_t remaining = len;
+    if (outgoing_.empty()) {
+        nbytes = socket_->send(data, len);
+        if (nbytes >= 0) {
+            remaining = len - nbytes;
+            if (remaining <= 0 && fn_on_write_) {
+                fn_on_write_(std::static_pointer_cast<connection>(shared_from_this()));
+                return nbytes;
+            }
+        } else {
+            // EAGAIN and EWOULDBLOCK can be ignored
+            // due to this is a non-blocking socket
+            if (errno != EAGAIN && errno != EWOULDBLOCK) {
+                sk_error("send error<%s>.", strerror(errno));
+                return -1;
+            }
+        }
+    }
+
+    sk_assert(remaining > 0 && remaining <= len);
+
+    outgoing_.append(sk::byte_offset<const void>(data, nbytes), remaining);
+
+    auto self(shared_from_this());
+    if (!reactor_->event_registered(self, reactor::EVENT_WRITABLE))
+        reactor_->register_handler(self, reactor::EVENT_WRITABLE);
+
+    return nbytes;
 }
 
 int connection::recv() {
-    // TODO
+    if (state_ != state_connected) {
+        sk_error("wrong state<%d>.", state_);
+        return -1;
+    }
+
+    auto self(shared_from_this());
+    if (!reactor_->event_registered(self, reactor::EVENT_READABLE))
+        reactor_->register_handler(self, reactor::EVENT_READABLE);
+
     return 0;
 }
 
+void connection::close() {
+    if (state_ == state_disconnected) return;
+
+    sk_debug("current state<%d>.", state_);
+    state_ = state_disconnected;
+
+    auto self(shared_from_this());
+    reactor_->deregister_handler(self, reactor::EVENT_READABLE | reactor::EVENT_WRITABLE);
+
+    if (fn_on_close_)
+        fn_on_close_(std::static_pointer_cast<connection>(self));
+}
+
 void connection::on_event(int event) {
-    // TODO
+    // TODO: what should we do for the two events?
+    if (event & reactor::EVENT_EPOLLERR)
+        sk_error("epollerr");
+    if (event & reactor::EVENT_EPOLLHUP)
+        sk_error("epollhup");
+
+    if (event & reactor::EVENT_WRITABLE) {
+        sk_assert(!outgoing_.empty());
+        ssize_t nbytes = socket_->send(outgoing_.peek(), outgoing_.size());
+        if (nbytes >= 0) {
+            outgoing_.consume(nbytes);
+            if (outgoing_.empty()) {
+                auto self(shared_from_this());
+                reactor_->deregister_handler(self, reactor::EVENT_WRITABLE);
+
+                if (fn_on_write_)
+                    fn_on_write_(std::static_pointer_cast<connection>(self));
+            }
+        } else {
+            sk_error("send error<%s>.", strerror(errno));
+        }
+    }
+
+    if (event & reactor::EVENT_READABLE) {
+        static char buf[8192];
+        ssize_t nbytes = socket_->recv(buf, sizeof(buf));
+        if (nbytes > 0) {
+            if (fn_on_message_) {
+                auto self(shared_from_this());
+                fn_on_message_(std::static_pointer_cast<connection>(self), &incoming_);
+            }
+        } else if (nbytes == 0) {
+            close();
+        } else {
+            sk_error("recv error<%s>.", strerror(errno));
+        }
+    }
 }
 
 NS_END(sk)
--- src/net/connection.h
+++ src/net/connection.h
@@ -2,30 +2,62 @@
 #define CONNECTION_H
 
 #include "net/socket.h"
+#include "net/buffer.h"
 #include "net/handler.h"
 
 NS_BEGIN(sk)
 
+class connection;
+typedef std::shared_ptr<connection> connection_ptr;
+
 class connection : public handler {
 public:
     MAKE_NONCOPYABLE(connection);
+    typedef std::function<void(const connection_ptr&)> fn_on_connection;
+    typedef std::function<void(const connection_ptr&)> fn_on_write;
+    typedef std::function<void(const connection_ptr&)> fn_on_close;
+    typedef std::function<void(const connection_ptr&, buffer*)> fn_on_message;
 
-    static handler_ptr create(reactor *r);
+    static connection_ptr create(reactor *r,
+                                 const socket_ptr& socket,
+                                 const inet_address& local_addr,
+                                 const inet_address& remote_addr);
     virtual ~connection() = default;
 
-    void set_socket(const socket_ptr& socket);
-
-    int send(const void *data, size_t len);
+    ssize_t send(const void *data, size_t len);
     int recv();
+    void close();
+
+    const inet_address& local_address() const { return local_addr_; }
+    const inet_address& remote_address() const { return remote_addr_; }
 
     virtual void on_event(int event) override;
     virtual int handle() const override { return socket_->fd(); }
 
 private:
-    connection(reactor *r) : handler(r) {}
+    connection(reactor *r,
+               const socket_ptr& socket,
+               const inet_address& local_addr,
+               const inet_address& remote_addr);
 
 private:
+    enum state {
+        state_disconnected,
+        state_connected
+    };
+
+private:
+    state state_;
     socket_ptr socket_;
+    inet_address local_addr_;
+    inet_address remote_addr_;
+    buffer incoming_;
+    buffer outgoing_;
+
+    fn_on_connection fn_on_connection_;
+    fn_on_write fn_on_write_;
+    fn_on_close fn_on_close_;
+    fn_on_message fn_on_message_;
 };
 
 NS_END(sk)
