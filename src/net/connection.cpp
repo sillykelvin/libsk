e942b36d995d8d9440cc072a97f306e66975fd10
Kelvin Hu <ini.kelvin@gmail.com>
2017-04-19 17:33:50 +0800

    update class connection

--- src/net/connection.cpp
+++ src/net/connection.cpp
@@ -6,25 +6,14 @@ NS_BEGIN(sk)
 
 connection_ptr connection::create(reactor *r,
                                   const socket_ptr& socket,
-                                  const inet_address& local_addr,
                                   const inet_address& remote_addr) {
-    return std::make_shared<connection>(r, socket, local_addr, remote_addr);
+    sk_assert(socket);
+    return connection_ptr(new connection(r, socket, remote_addr));
 }
 
-connection::connection(reactor *r,
-                       const socket_ptr& socket,
-                       const inet_address& local_addr,
-                       const inet_address& remote_addr)
-    : handler(r),
-      state_(state_disconnected),
-      socket_(socket),
-      local_addr_(local_addr),
-      remote_addr_(remote_addr)
-{}
-
 ssize_t connection::send(const void *data, size_t len) {
-    if (state_ != state_connected) {
-        sk_error("wrong state<%d>.", state_);
+    if (!socket_) {
+        sk_error("invalid socket.");
         return -1;
     }
 
@@ -54,33 +43,36 @@ ssize_t connection::send(const void *data, size_t len) {
     outgoing_.append(sk::byte_offset<const void>(data, nbytes), remaining);
 
     auto self(shared_from_this());
-    if (!reactor_->event_registered(self, reactor::EVENT_WRITABLE))
-        reactor_->register_handler(self, reactor::EVENT_WRITABLE);
+    if (!reactor_->writing_enabled(self))
+        reactor_->enable_writing(self);
 
     return nbytes;
 }
 
 int connection::recv() {
-    if (state_ != state_connected) {
-        sk_error("wrong state<%d>.", state_);
+    if (!socket_) {
+        sk_error("invalid socket.");
         return -1;
     }
 
     auto self(shared_from_this());
-    if (!reactor_->event_registered(self, reactor::EVENT_READABLE))
-        reactor_->register_handler(self, reactor::EVENT_READABLE);
+    if (!reactor_->reading_enabled(self))
+        reactor_->enable_reading(self);
 
     return 0;
 }
 
 void connection::close() {
-    if (state_ == state_disconnected) return;
+    if (!socket_) return;
 
-    sk_debug("current state<%d>.", state_);
-    state_ = state_disconnected;
+    if (!outgoing_.empty())
+        sk_warn("output queue is NOT empty!");
 
     auto self(shared_from_this());
-    reactor_->deregister_handler(self, reactor::EVENT_READABLE | reactor::EVENT_WRITABLE);
+    reactor_->disable_all(self);
+
+    // TODO: better handling here than just forcly close the socket fd
+    socket_.reset();
 
     if (fn_on_close_)
         fn_on_close_(std::static_pointer_cast<connection>(self));
@@ -100,27 +92,32 @@ void connection::on_event(int event) {
             outgoing_.consume(nbytes);
             if (outgoing_.empty()) {
                 auto self(shared_from_this());
-                reactor_->deregister_handler(self, reactor::EVENT_WRITABLE);
+                reactor_->disable_writing(self);
 
                 if (fn_on_write_)
                     fn_on_write_(std::static_pointer_cast<connection>(self));
             }
         } else {
+            // TODO: disable writing here?
             sk_error("send error<%s>.", strerror(errno));
         }
     }
 
     if (event & reactor::EVENT_READABLE) {
-        static char buf[8192];
+        static char buf[65536]; // 64KB
         ssize_t nbytes = socket_->recv(buf, sizeof(buf));
         if (nbytes > 0) {
-            if (fn_on_message_) {
+            incoming_.append(buf, nbytes);
+            if (fn_on_read_) {
                 auto self(shared_from_this());
-                fn_on_message_(std::static_pointer_cast<connection>(self), &incoming_);
+                fn_on_read_(std::static_pointer_cast<connection>(self), &incoming_);
             }
         } else if (nbytes == 0) {
+            // TODO: a better handler here
+            reactor_->disable_reading(shared_from_this());
             close();
         } else {
+            // TODO: disable reading here?
             sk_error("recv error<%s>.", strerror(errno));
         }
     }
--- src/net/connection.h
+++ src/net/connection.h
@@ -13,14 +13,12 @@ typedef std::shared_ptr<connection> connection_ptr;
 class connection : public handler {
 public:
     MAKE_NONCOPYABLE(connection);
-    typedef std::function<void(const connection_ptr&)> fn_on_connection;
+    typedef std::function<void(const connection_ptr&, buffer*)> fn_on_read;
     typedef std::function<void(const connection_ptr&)> fn_on_write;
     typedef std::function<void(const connection_ptr&)> fn_on_close;
-    typedef std::function<void(const connection_ptr&, buffer*)> fn_on_message;
 
     static connection_ptr create(reactor *r,
                                  const socket_ptr& socket,
-                                 const inet_address& local_addr,
                                  const inet_address& remote_addr);
     virtual ~connection() = default;
 
@@ -28,8 +26,11 @@ public:
     int recv();
     void close();
 
-    const inet_address& local_address() const { return local_addr_; }
     const inet_address& remote_address() const { return remote_addr_; }
+
+    void set_read_callback (const fn_on_read&  fn) { fn_on_read_  = fn; }
+    void set_write_callback(const fn_on_write& fn) { fn_on_write_ = fn; }
+    void set_close_callback(const fn_on_close& fn) { fn_on_close_ = fn; }
 
     virtual void on_event(int event) override;
     virtual int handle() const override { return socket_->fd(); }
@@ -37,27 +38,18 @@ public:
 private:
     connection(reactor *r,
                const socket_ptr& socket,
-               const inet_address& local_addr,
-               const inet_address& remote_addr);
+               const inet_address& remote_addr)
+        : handler(r), socket_(socket), remote_addr_(remote_addr) {}
 
 private:
-    enum state {
-        state_disconnected,
-        state_connected
-    };
-
-private:
-    state state_;
     socket_ptr socket_;
-    inet_address local_addr_;
     inet_address remote_addr_;
     buffer incoming_;
     buffer outgoing_;
 
-    fn_on_connection fn_on_connection_;
+    fn_on_read  fn_on_read_;
     fn_on_write fn_on_write_;
     fn_on_close fn_on_close_;
-    fn_on_message fn_on_message_;
 };
 
 NS_END(sk)
