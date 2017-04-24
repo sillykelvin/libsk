a3b34830bb9a7534737dd757398cff60a0584c83
Kelvin Hu <ini.kelvin@gmail.com>
2017-04-24 16:12:49 +0800

    add callback definitions

---
+++ src/net/callback.h
@@ -0,0 +1,21 @@
+#ifndef CALLBACK_H
+#define CALLBACK_H
+
+#include <functional>
+#include <memory>
+#include "utility/types.h"
+
+NS_BEGIN(sk)
+
+class buffer;
+class tcp_connection;
+typedef std::shared_ptr<tcp_connection> tcp_connection_ptr;
+
+typedef std::function<void()> fn_on_poll_event;
+typedef std::function<void(int)> fn_on_error_event;
+typedef std::function<void(const tcp_connection_ptr&)> fn_on_connection_event;
+typedef std::function<void(const tcp_connection_ptr&, buffer*)> fn_on_connection_message;
+
+NS_END(sk)
+
+#endif // CALLBACK_H
