000c08078f633a7668fd69b9dabfa0466c5048e1
Kelvin Hu <ini.kelvin@gmail.com>
2017-04-26 18:44:31 +0800

    add missing files

---
+++ src/net/detail/handler.cpp
@@ -0,0 +1,70 @@
+#include "handler.h"
+#include "net/reactor.h"
+#include "utility/assert_helper.h"
+
+NS_BEGIN(sk)
+NS_BEGIN(net)
+NS_BEGIN(detail)
+
+handler::handler(reactor *r, int fd)
+    : fd_(fd), events_(EVENT_NONE), reactor_(r) {}
+
+handler::~handler() {
+    sk_assert(!reactor_->handler_registered(this));
+}
+
+void handler::on_event(int events) {
+    sk_trace("RD(%d) WR(%d) ERR(%d) HUP(%d)",
+             events & EVENT_READABLE ? 1 : 0, events & EVENT_WRITABLE ? 1 : 0,
+             events & EVENT_EPOLLERR ? 1 : 0, events & EVENT_EPOLLHUP ? 1 : 0);
+
+    if (fd_ == -1) {
+        sk_error("handler already invalidated!");
+        disable_all();
+        return;
+    }
+
+    if (events & (EVENT_READABLE | EVENT_EPOLLHUP | EVENT_EPOLLERR)) {
+        if (fn_on_read_) fn_on_read_();
+        if (fd_ == -1) return;
+    }
+
+    if (events & (EVENT_WRITABLE | EVENT_EPOLLHUP | EVENT_EPOLLERR)) {
+        if (fn_on_write_) fn_on_write_();
+        if (fd_ == -1) return;
+    }
+}
+
+void handler::enable(int event) {
+    if (fd_ == -1) {
+        sk_error("handler invalidated!");
+        return;
+    }
+
+    if (events_ & event)
+        return;
+
+    events_ |= event;
+    update();
+}
+
+void handler::disable(int event) {
+    if (fd_ == -1) {
+        sk_error("handler invalidated!");
+        return;
+    }
+
+    if (!(events_ & event))
+        return;
+
+    events_ &= ~event;
+    update();
+}
+
+void handler::update() {
+    reactor_->register_handler(this);
+}
+
+NS_END(detail)
+NS_END(net)
+NS_END(sk)
---
+++ src/net/detail/handler.h
@@ -0,0 +1,61 @@
+#ifndef HANDLER_H
+#define HANDLER_H
+
+#include "net/callback.h"
+
+NS_BEGIN(sk)
+NS_BEGIN(net)
+class reactor;
+NS_BEGIN(detail)
+
+class handler {
+public:
+    // TODO: merge these events with epoll events
+    static const int EVENT_NONE     = 0x00;
+    static const int EVENT_READABLE = 0x01;
+    static const int EVENT_WRITABLE = 0x02;
+    static const int EVENT_EPOLLERR = 0x04;
+    static const int EVENT_EPOLLHUP = 0x08;
+
+    MAKE_NONCOPYABLE(handler);
+
+    handler(reactor *r, int fd);
+    ~handler();
+
+    void on_event(int events);
+
+    int fd() const { return fd_; }
+    int events() const { return events_; }
+    bool has_event() const { return events_ != EVENT_NONE; }
+
+    void invalidate() { disable_all(); fd_ = -1; }
+
+    void enable_reading()  { enable(EVENT_READABLE);  }
+    void enable_writing()  { enable(EVENT_WRITABLE);  }
+    void disable_reading() { disable(EVENT_READABLE); }
+    void disable_writing() { disable(EVENT_WRITABLE); }
+    void disable_all()     { disable(EVENT_READABLE | EVENT_WRITABLE); }
+    bool reading_enabled() const { return events_ & EVENT_READABLE; }
+    bool writing_enabled() const { return events_ & EVENT_WRITABLE; }
+
+    void on_read_event (const fn_on_poll_event& fn) { fn_on_read_  = fn; }
+    void on_write_event(const fn_on_poll_event& fn) { fn_on_write_ = fn; }
+
+private:
+    void enable(int event);
+    void disable(int event);
+    void update();
+
+private:
+    int fd_;
+    int events_;
+    reactor *reactor_;
+    fn_on_poll_event fn_on_read_;
+    fn_on_poll_event fn_on_write_;
+};
+
+NS_END(detail)
+NS_END(net)
+NS_END(sk)
+
+#endif // HANDLER_H
