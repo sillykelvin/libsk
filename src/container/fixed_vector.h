65fa3078c9107f0050b8e65cd531d48e7b351b74
Kelvin Hu <ini.kelvin@gmail.com>
2017-12-15 14:38:58 +0800

    minor improvement to fixed_vector

--- src/container/fixed_vector.h
+++ src/container/fixed_vector.h
@@ -2,58 +2,42 @@
 #define FIXED_VECTOR_H
 
 #include <algorithm>
-#include "utility/types.h"
+#include <utility/assert_helper.h>
 
-namespace sk {
+NS_BEGIN(sk)
 
-/*
- * fixed_vector is a vector with fixed size, can be used
- * in both normal memory and shared memory.
- *
- * TODO: implement a specialization version for bool
- */
 template<typename T, size_t N>
-struct fixed_vector {
-    typedef T* iterator;
+class fixed_vector {
+public:
+    typedef T*       iterator;
     typedef const T* const_iterator;
 
-    size_t used_count;
-    char memory[sizeof(T) * N];
-
-    fixed_vector() : used_count(0) {}
+public:
     ~fixed_vector() { clear(); }
+    fixed_vector() : size_(0), peak_size_(0) {}
 
-    fixed_vector(const fixed_vector& vector) : used_count(0) {
-        const_iterator it, end;
-        for (it = vector.begin(), end = vector.end(); it != end; ++it)
+    fixed_vector(const fixed_vector& vector) : size_(0), peak_size_(0) {
+        for (const_iterator it = vector.begin(), end = vector.end(); it != end; ++it)
             emplace(*it);
     }
 
     fixed_vector& operator=(const fixed_vector& vector) {
-        if (this == &vector)
-            return *this;
+        if (this == &vector) return *this;
 
         clear();
 
-        const_iterator it, end;
-        for (it = vector.begin(), end = vector.end(); it != end; ++it)
+        for (const_iterator it = vector.begin(), end = vector.end(); it != end; ++it)
             emplace(*it);
 
         return *this;
     }
 
-    size_t size()     const { return used_count; }
-    size_t capacity() const { return N; }
-
-    bool full()  const { return used_count >= N; }
-    bool empty() const { return used_count <= 0; }
-
+public:
     void clear() {
-        iterator it, end;
-        for (it = this->begin(), end = this->end(); it != end; ++it)
+        for (iterator it = this->begin(), end = this->end(); it != end; ++it)
             it->~T();
 
-        used_count = 0;
+        size_ = 0;
     }
 
     void fill(size_t n, const T& value) {
@@ -69,43 +53,39 @@ struct fixed_vector {
         }
 
         std::fill(begin(), at(n), value);
-        for (size_t i = used_count - 1; i >= n; --i)
+        for (size_t i = size_ - 1; i >= n; --i)
             erase_at(i);
     }
 
     iterator at(size_t index) {
-        assert_retval(index < used_count, end());
-
+        assert_retval(index < size_, end());
         return begin() + index;
     }
 
     const_iterator at(size_t index) const {
-        assert_retval(index < used_count, end());
-
+        assert_retval(index < size_, end());
         return begin() + index;
     }
 
     void erase_at(size_t index) {
-        assert_retnone(index < used_count);
+        assert_retnone(index < size_);
 
         if (at(index) + 1 != end())
             std::copy(at(index) + 1, end(), at(index));
 
-        at(used_count - 1)->~T();
-        --used_count;
+        at(size_ - 1)->~T();
+        --size_;
     }
 
     void erase(iterator it) {
         assert_retnone(it != end());
-
         size_t index = it - begin();
         erase_at(index);
     }
 
     void erase(const T& value) {
         iterator it = find(value);
-        if (it == end())
-            return;
+        if (it == end()) return;
 
         erase(it);
     }
@@ -113,19 +93,20 @@ struct fixed_vector {
     template<typename Pred>
     void erase_if(Pred p) {
         iterator it = find_if(p);
-        if (it == end())
-            return;
+        if (it == end()) return;
 
         erase(it);
     }
 
     template<typename... Args>
     T* emplace(Args&&... args) {
-        if (full())
-            return NULL;
+        if (full()) return nullptr;
 
-        T *t = at(used_count++);
+        T *t = at(size_++);
         new (t) T(std::forward<Args>(args)...);
+
+        if (size_ > peak_size_)
+            peak_size_ = size_;
 
         return t;
     }
@@ -138,32 +119,17 @@ struct fixed_vector {
         return *at(index);
     }
 
-    const_iterator find(const T& value) const {
-        const_iterator it, end;
-        for (it = this->begin(), end = this->end(); it != end; ++it) {
-            if (*it == value)
-                return it;
-        }
-
-        return this->end();
-    }
-
     iterator find(const T& value) {
-        iterator it, end;
-        for (it = this->begin(), end = this->end(); it != end; ++it) {
-            if (*it == value)
-                return it;
+        for (iterator it = this->begin(), end = this->end(); it != end; ++it) {
+            if (*it == value) return it;
         }
 
         return this->end();
     }
 
-    template<typename Pred>
-    const_iterator find_if(Pred p) const {
-        const_iterator it, end;
-        for (it = this->begin(), end = this->end(); it != end; ++it) {
-            if (p(*it))
-                return it;
+    const_iterator find(const T& value) const {
+        for (const_iterator it = this->begin(), end = this->end(); it != end; ++it) {
+            if (*it == value) return it;
         }
 
         return this->end();
@@ -171,21 +137,39 @@ struct fixed_vector {
 
     template<typename Pred>
     iterator find_if(Pred p) {
-        iterator it, end;
-        for (it = this->begin(), end = this->end(); it != end; ++it) {
-            if (p(*it))
-                return it;
+        for (iterator it = this->begin(), end = this->end(); it != end; ++it) {
+            if (p(*it)) return it;
         }
 
         return this->end();
     }
 
-    iterator begin() { return cast_ptr(T, memory); }
-    iterator end()   { return cast_ptr(T, memory) + used_count; }
-    const_iterator begin() const { return reinterpret_cast<const T*>(memory); }
-    const_iterator end()   const { return reinterpret_cast<const T*>(memory) + used_count; }
+    template<typename Pred>
+    const_iterator find_if(Pred p) const {
+        for (const_iterator it = this->begin(), end = this->end(); it != end; ++it) {
+            if (p(*it)) return it;
+        }
+
+        return this->end();
+    }
+
+    iterator begin() { return cast_ptr(T, memory_); }
+    iterator end()   { return cast_ptr(T, memory_) + size_; }
+    const_iterator begin() const { return reinterpret_cast<const T*>(memory_); }
+    const_iterator end()   const { return reinterpret_cast<const T*>(memory_) + size_; }
+
+    bool full()  const { return size_ >= N; }
+    bool empty() const { return size_ <= 0; }
+    size_t size()     const { return size_; }
+    size_t capacity() const { return N; }
+    size_t peak_size() const { return peak_size_; }
+
+private:
+    size_t size_;
+    size_t peak_size_;
+    char memory_[sizeof(T) * N];
 };
 
-} // namespace sk
+NS_END(sk)
 
 #endif // FIXED_VECTOR_H
